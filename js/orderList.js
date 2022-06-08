cca77c0809706eab9a5fcaf768e9787e73329d43
lyq <1186432416@qq.com>
2022-06-08 16:59:42 +0800

    饿了吗前端

--- js/orderList.js
+++ js/orderList.js
@@ -1,9 +1,9 @@
 window.onload = function(){
-	//获取显示隐藏按钮DOM数组
+
 	let showBtnArr = document.getElementsByClassName('fa-caret-down');
-	//获取订单明细DOM数组
+
 	let detailetBoxArr = document.getElementsByClassName('order-detailet');
-	//设置默认所有订单明细都隐藏
+
 	for(let i=0;i<detailetBoxArr.length;i++){
 		detailetBoxArr[i].style.display='none';
 	}
